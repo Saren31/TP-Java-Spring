0467e86ff2390cc8b8b5071bf8b4f80026783bc4
vsl4346a <lucas.veslin@etu.iut-tlse3.fr>
2023-11-21 10:17:50 +0100

    hello-world_update

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
 
-	@GetMapping("/")
+	@GetMapping("/bonjour")
 	public String index() {
 		return "Bonjour le monde !";
 	}
